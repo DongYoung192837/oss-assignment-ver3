23c51bf68fcbd96239c40bfbd2dcaa319e4c779a
Kim Dong Young <lovecom12340@naver.com>
2025-11-27 07:42:58 +0000

    F

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Hello{
     public static void main(String[] args){
         System.out.println("Hello! Java! and git!");        
-        System.out.println("Test1");::
+        System.out.println("Test1");
+        System.out.println("Test2");
     }
 }
